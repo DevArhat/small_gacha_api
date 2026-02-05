a8b1ef7a39701aae2481a7789c4ed8336658e42c
Arhat <sososlollr@gmail.com>
2026-02-05 17:38:06 +0900

    원신 별빛포착 로직 추가, 스타레일 로직 코드 작성

--- simulator/games/hoyoverse/genshin.py
+++ simulator/games/hoyoverse/genshin.py
@@ -8,6 +8,11 @@ import random
 # 반천 50:50
 # 별빛 포착: 반천 픽뚫을 3번 당하면 다음 반천을 확천으로 바꿔주는 시스템이라고 함....
 # 확정적으로 확률공지가 나온 시스템이 아니라서 좀더 찾아봐야될듯
+# 지금까지 커뮤니티에서 분석된 내용:
+# 반천장 5성 캐릭터 뽑기에 진입하는 순간 기준,
+# 0스택 0.018%, 1스택 5%, 2스택 10%, 3스택 확정 (1/2스택일 때의 확률 불명)
+# 확천이 아닐 때 픽업 캐릭터를 획득하면 1스택 감소 (0 미만으로 감소하지 않음)
+# 픽뚫이 나면 1스택 증가
 
 # 4성 관련 확률 정리
 # 기본확률 5.1%
@@ -15,7 +20,7 @@ import random
 # 반천-확천 사이클 있음
 
 # 공통사항
-# 캐릭터 명함 -> 뽑기 환급재료 지급 없음
+# 캐릭터 최초획득 -> 뽑기 환급재료 지급 없음
 # 5성 중복 캐릭터 -> 스타라이트 10개
 # 4성 중복 캐릭터 -> 스타라이트 2개
 # 5성 풀돌 캐릭터 -> 스타라이트 25개
@@ -23,8 +28,8 @@ import random
 # 4성 무기 -> 스타라이트 2개
 # 3성 무기 -> 스타더스트 15개
 # 5성 픽뚫은 중복으로 가정
-# 4성 확률업 캐릭터는 풀돌, 아닌 캐릭터는 단순중복으로 가정
-# 스타더스트 뽑기재화는 월간 5개만 교환 가능하므로 굳이 고려하지 않았음
+# 4성 캐릭터는 풀돌 상태로 가정
+# 스타더스트 뽑기권은 월간 5개만 교환 가능하므로 굳이 고려하지 않았음
 
 # 교환비
 # 스타라이트 5개 -> 뽑기권 1
@@ -52,18 +57,28 @@ class GenshinImpact(Game):
         stack_4 = 0
         guaranteed = False
         
+        # 별빛 포착 스택용 변수
+        # 이 시뮬레이터에서 별빛포착 확률 모델은
+        # https://www.reddit.com/r/Genshin_Impact/comments/1hd1sqa/understanding_genshin_impacts_capturing_radiance/
+        # https://preview.redd.it/understanding-genshin-impacts-capturing-radiance-in-depth-v0-a640fhy4yi6e1.png?width=1080&crop=smart&auto=webp&s=9d2d714427299718b0296c55ae3c9dd0d64975e6
+        # 위의 reddit 글과 이미지를 기반으로 했음
+        # 해당 모델에 따르면, 스택은 0~3 범위에서 동작하지만 기본값은 1임
+        capturing_radiance_stack = 1
+        
         while stats["pickup_5"] < target_copies:
             stats["total_pulls"] += 1
             stack_5 += 1
             stack_4 += 1
             
+            # 이번 회차의 랜덤 숫자 발급
+            curr_random = random.random()
             # 5성 확률 계산
             rate_5 = 0.006
             if stack_5 >= 74:
                 rate_5 = 0.006 + (stack_5 - 73) * 0.06
             
             # 5성 획득 판정
-            if stack_5 == 90 or random.random() < rate_5:
+            if stack_5 == 90 or curr_random < rate_5:
                 stack_5 = 0
                 is_pickup = False
                 
@@ -73,34 +88,104 @@ class GenshinImpact(Game):
                 else:
                     if random.choice([True, False]):
                         is_pickup = True
-                    else:
-                        guaranteed = True
-                
+                        capturing_radiance_stack = minus_stack(capturing_radiance_stack)
+                    else:                          
+                        radiance_result = handle_radiance(capturing_radiance_stack)
+                        is_pickup, capturing_radiance_stack = radiance_result
+                        if is_pickup:
+                            stats["pickup_5"] += 1
+                            if int(stats["pickup_5"]) == 1:
+                                stats["log"].append(f"[Pull {stats['total_pulls']}] 별빛 포착! 5★ 픽업 획득 (현재 명함)")
+                            else:
+                                stats["log"].append(f"[Pull {stats['total_pulls']}] 별빛 포착! 5★ 픽업 획득 (현재 {int(stats['pickup_5'])}번 획득: {int(stats['pickup_5'])-1}돌)")
+                                stats["crumbs"] += 10
+                        else:
+                            stats["other_5"] += 1
+                            stats["log"].append(f"[Pull {stats['total_pulls']}] 5★ 상시 획득 (픽뚫)")
+                            stats["crumbs"] += 10
+                            guaranteed = True
+                        continue
+                            
                 if is_pickup:
                     stats["pickup_5"] += 1
                     if int(stats["pickup_5"]) == 1:
                         stats["log"].append(f"[Pull {stats['total_pulls']}] 5★ 픽업 획득 (현재 명함)")
                     else:
                         stats["log"].append(f"[Pull {stats['total_pulls']}] 5★ 픽업 획득 (현재 {int(stats['pickup_5'])}번 획득: {int(stats['pickup_5'])-1}돌)")
+                        stats["crumbs"] += 10
                 else:
                     stats["other_5"] += 1
                     stats["log"].append(f"[Pull {stats['total_pulls']}] 5★ 상시 획득 (픽뚫)")
+                    stats["crumbs"] += 10
                 continue
             
             # 4성 획득 판정 (10회 천장)
-            # 5.1% 확률, 10회차에 확정,
-            # 반천 확천 사이클 있음, 5.1%를 캐릭터랑 무기가 반반 나눠가짐
+            # 5.1% 확률, 10회차에 확정
+            # 반천 확천 사이클 있음, 확업4성캐랑 기타4성 전체가 반반 나눠가짐
             # 4성 획득 시 50% 확률로 확률업 4성
-            # 근데 이 말대로면 캐릭터가 나오는 파이를 확률업 캐릭터가 다 먹고 있는데,
-            # 4성에도 캐릭터 확률이 점점 올라가는 로직이 있나?
-            # 그렇게 되면 만약에 캐릭터가 3%, 무기가 2.55% 이렇게 올라갔다고 쳤을 때
-            # 4성 획득 확률에서 50%를 떼어내도 캐릭터쪽에 파이가 좀 있으니까, 확률업이 아닌 4성 캐가 나오는게 설명이 되지 않나 싶음..
+            # 위의 내용은 무시하고 모든 4성 캐릭터는 풀돌 상태인 걸로 가정
+            # 4성 아이템 비중에서 무기랑 캐릭터가 반반갈라 파이 먹고 있다고 치고
+            # 4성 획득 중에서 25% 확률로는 무기가 떠서 스타라이트가 2개만 나왔다고 시뮬레이트
+            # 더 좋은 예측 모델이 있으면 스타라이트 관련된 부분은 수정 필요함
             rate_4 = 0.051
-            if stack_4 >= 10 or random.random() < rate_4:
+            if stack_4 >= 10 or curr_random < rate_4:
                 stats["4_star"] += 1
+                stats["crumbs"] += 5
                 stack_4 = 0
+                # 무기가 떴는지 캐릭터가 떴는지 판단하는 랜덤숫자는 메인 뽑기 확률과 별개
+                if random.random() < 0.25:
+                    stats["crumbs"] -= 3
                 continue
             # 나머지는 3성 무기
             stats["weapon_3"] += 1
                 
         return stats
+
+def minus_stack(radiance_stack) -> int:
+    """
+    별빛포착 스택을 빼기 위한 함수
+    본 시뮬레이션에 적용한 모델에 따르면,
+    기본값은 1이며 반천 픽업에 성공할 때마다 스택이 내려가지만 0 미만으로 내려가지는 않으므로,
+    반복적인 사용을 위해 capturing_radiance_stack -= 1 로 계산하지 않고 별도 함수를 사용함
+
+    Args:
+        radiance_stack (_int_): 원본 별빛포착 스택
+
+    Returns:
+        new_stack (_int_): 뺄셈 진행 후 별빛포착 스택 (최소치 0)
+    """
+    if radiance_stack <= 0:
+        return radiance_stack
+    else:
+        return radiance_stack - 1
+
+def handle_radiance(radiance_stack: int) -> tuple[bool, int]:
+    """
+    입력된 radiance stack에 따라 별빛포착 랜덤발동 성공 여부와 새로운 radiance stack을 반환하는 함수
+    
+    :param radiance_stack: 현재 별빛포착 스택
+    :type radiance_stack: int
+    :return: 별빛포착 발동 여부 , 계산 후 별빛포착 스택
+    :rtype: is_radiance_captured, new_stack: tuple[bool, int]
+    """
+    if radiance_stack == 0:
+        if random.random() < 0.00018:
+            return (True, 0)
+        else: 
+            return (False, 1)
+    elif radiance_stack == 1:
+        if random.random() < 0.05:
+            return (True, 0)
+        else:
+            return (False, radiance_stack+1)
+    elif radiance_stack == 2:
+        if random.random() < 0.1:
+            return (True, 0)
+        else:
+            return (False, radiance_stack+1)
+    elif radiance_stack == 3:
+        return (True, 1)
+    else:
+        return (False, radiance_stack)
+    
+        
